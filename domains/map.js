eba0d755a51ca37600fbcb4c7dcfaea6ce62cd5f
Prasoon Gupta <itsprasoon2004@gmail.com>
2026-01-11 18:31:44 +0530

    Create map.js

---
+++ domains/map.js
@@ -0,0 +1,28 @@
+// domain-map.js
+window.DOMAIN_MAP = {
+  marriage: [
+    "marriage",
+    "shaadi",
+    "shadi",
+    "sadi",
+    "vivaah",
+    "vivah",
+    "wedding"
+  ],
+
+  exam: [
+    "exam",
+    "exams",
+    "paper",
+    "test",
+    "pariksha"
+  ],
+
+  money: [
+    "money",
+    "paisa",
+    "paise",
+    "finance",
+    "loan"
+  ]
+};
